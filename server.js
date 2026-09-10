efd6ec04095d2b8c2dee809a8d03f45d82a2d5a2
h15921209-collab <superby08@gmail.com>
2026-09-10 21:24:04 +0800

    chore(release): v2.1.0 - Neural Dual-Anchor Voice Synthesis Engine

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('X-Voice-Engine', 'Neural-Dual-Anchor-v2.1');
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
